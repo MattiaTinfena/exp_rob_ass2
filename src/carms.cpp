f028e8432904bc0de1eb2ab0c1e5471e4bf74409
MattiaTinfena <mattia.tinfena@gmail.com>
2026-01-17 11:24:20 +0100

    Publish id and center of the marker correctly

--- src/carms.cpp
+++ src/carms.cpp
@@ -6,6 +6,7 @@
 #include "sensor_msgs/msg/image.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/aruco.hpp>
+#include "geometry_msgs/msg/point.hpp"
 
 // void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg){
 //     try {
@@ -18,6 +19,8 @@
 //     }
 // }
 
+rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr publisher;
+
 void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg){
   try {
     cv::Mat img = cv_bridge::toCvShare(msg, "bgr8")->image;
@@ -26,13 +29,31 @@ void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg){
     cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_ARUCO_ORIGINAL);
     cv::aruco::detectMarkers(img, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
-    for(int i = 0; i < markerIds.size(); i++){
-      std::cout << "Marker: " << markerIds[i] << ", Corners: ";
-      for(int j = 0; j < markerCorners.size(); j++){
-        std::cout << markerCorners[i][j] << ",";
-      }
-      std::cout << std::endl;
+    
+    geometry_msgs::msg::Point message;
+
+    if(markerIds.size() == 1 ){
+        message.z = markerIds[0];
+        double x_center = 0, y_center = 0;
+
+        for(int i = 0; i < markerCorners[0].size(); i++){
+            x_center += markerCorners[0][i].x;
+            y_center += markerCorners[0][i].y;
+        }
+        message.x = x_center/4.0;
+        message.y = y_center/4.0;
+        publisher->publish(message);
     }
+
+
+
+    // for(int i = 0; i < markerIds.size(); i++){
+    //   std::cout << "Marker: " << markerIds[i] << ", Corners: ";
+    //   for(int j = 0; j < markerCorners.size(); j++){
+    //     std::cout << markerCorners[i][j] << ",";
+    //   }
+    //   std::cout << std::endl;
+    // }
   } catch (cv_bridge::Exception & e) {
     RCLCPP_ERROR(rclcpp::get_logger("subscriber"), "cv_bridge exception: %s", e.what());
     return;
@@ -45,12 +66,15 @@ int main(int argc, char ** argv){
     rclcpp::NodeOptions options;
     rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("image_listener", options);
     node->declare_parameter<std::string>("image_transport", "raw");
-    cv::namedWindow("view");
-    cv::startWindowThread();
+    // cv::namedWindow("view");
+    // cv::startWindowThread();
     image_transport::ImageTransport it(node);
     image_transport::TransportHints hints(node.get());
     image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback, &hints);
+    
+    publisher = node->create_publisher<geometry_msgs::msg::Point>("/id_detect", 10); // coordinates x and y for the center of the marker, z coordinate for the id
+
     rclcpp::spin(node);
-    cv::destroyWindow("view");
+    // cv::destroyWindow("view");
     return 0;
 }
